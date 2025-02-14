3d447352e85db5f933b9e6e4fc40836c791221f3
Kayxue <s0990006@gmail.com>
2025-02-14 18:30:01 +0800

    Init

---
+++ dockerfile
@@ -0,0 +1,16 @@
+FROM rust:slim AS build
+
+WORKDIR src
+COPY . .
+
+RUN USER=root apk add libc-dev
+RUN cargo build --release
+
+FROM alpine:latest
+WORKDIR src
+COPY --from=build /src/target/release .
+COPY --from=build /src/public ./public
+
+EXPOSE 3000
+
+CMD ./release/actixfuwariserve
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use actix_files as fs;
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(fs::Files::new("/", "./public").index_file("index.html")))
+        .bind(("0.0.0.0", 3000))?
+        .run()
+        .await
+}
